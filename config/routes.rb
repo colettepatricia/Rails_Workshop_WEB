efed1a26e3970771a1020618f6143376e1a0132c
Colette <colls1795@gmail.com>
2015-02-24 15:03:35 +0530

    done

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  get 'home/home'
 
+  resources :blogs
+
+  get "home" => "home#home"
+  get "about" => "home#about"
+  get "cont"  => "home#contacts"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
